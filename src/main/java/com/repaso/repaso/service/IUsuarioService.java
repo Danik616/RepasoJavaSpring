96d755269f3b84d3aa4c11c8d010a9b55a50b862
Danik616 <ddanielvargas2011@gmail.com>
2023-04-25 15:34:01 -0500

    Se elimina información no relevante en el servicio del usuario

--- src/main/java/com/repaso/repaso/service/IUsuarioService.java
+++ src/main/java/com/repaso/repaso/service/IUsuarioService.java
@@ -2,7 +2,6 @@ package com.repaso.repaso.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-import com.repaso.repaso.entity.Usuario;
 
 public interface IUsuarioService extends UserDetailsService{
     
